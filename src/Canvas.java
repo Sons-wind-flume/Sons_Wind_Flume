8359d4ce45f20088ff43ba2fcabb0b858f2d1463
tom fox <thomas.fox1@students.mq.edu.au>
2026-09-16 22:10:00 +1000

    implemented phidget gate class and ability to get median from gathered data

---
+++ src/Canvas.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+public class Canvas extends JFrame {
+
+}
---
+++ src/phidgetGate.java
@@ -0,0 +1,52 @@
+import com.phidget22.*;
+import java.util.ArrayList;
+import java.io.IOException;
+public class phidgetGate{
+    VoltageRatioInput voltageRatioInput;
+    ArrayList<Double> values;
+    int ChannelNum;
+    public phidgetGate(int phidgetChannelNum){
+        ChannelNum=phidgetChannelNum;
+        try {
+            voltageRatioInput.setChannel(phidgetChannelNum);
+            voltageRatioInput.open(5000);
+        } catch (Exception e) { 
+        }
+        
+        voltageRatioInput.addVoltageRatioChangeListener((VoltageRatioInputVoltageRatioChangeEvent e) -> {
+			values.add(e.getVoltageRatio());
+		});
+
+        try {
+            System.in.read();
+            voltageRatioInput.close();
+        } catch (Exception e) {
+        }
+    }
+
+    
+    public void sort(){
+        //todo
+    }
+
+    public double getMedianValue(){
+        if (values==null||values.isEmpty()) {
+            return 0;
+        }
+        sort();
+        int len=values.size();
+        double median=0;
+        if(len%2!=0){
+            median=values.get(len/2);
+        }
+        else{
+            median= (values.get((len / 2) - 1) + values.get(len / 2)) / 2.0;
+        }
+        resetValues();
+        return median;
+    }
+    public void resetValues(){
+        values = new ArrayList<>();
+    }
+
+}
